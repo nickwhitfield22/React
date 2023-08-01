7ee3e9d784cb6a2ff41f9548560f7ec84e8a148b
nickwhitfield22 <nick.b.whitfield@gmail.com>
2023-07-31 20:26:13 -0400

    new stuff

---
+++ react-challenge-dev-counter/src/App.js
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Counter />
+    </div>
+  );
+}
+
+function Counter() {
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+
+  const date = new Date();
+  date.setDate(date.getDate() + count);
+
+  function handleStep(e) {
+    setStep(Number(e.target.value));
+  }
+
+  function handleReset() {
+    setCount(0);
+    setStep(1);
+  }
+
+  return (
+    <div>
+      <div>
+        <input
+          type="range"
+          min="0"
+          max="10"
+          defaultValue={step}
+          onChange={handleStep}
+        />
+        <label>{step}</label>
+      </div>
+      <div>
+        <button onClick={() => setCount((c) => c - step)}>-</button>
+        <input
+          value={count}
+          onChange={(e) => setCount(Number(e.target.value))}
+        />
+        <button onClick={() => setCount((c) => c + step)}>+</button>
+      </div>
+      <p>
+        <span>
+          {count === 0
+            ? "Today is "
+            : count > 0
+            ? `${count} days from today is `
+            : `${Math.abs(count)} days ago was `}
+        </span>
+        <span>{date.toDateString()}</span>
+      </p>
+      <button onClick={handleReset}>Reset</button>
+    </div>
+  );
+}
